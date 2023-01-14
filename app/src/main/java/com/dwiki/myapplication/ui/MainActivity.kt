aa2143a4760c6d109328f6259f43a38f2741fff9
DWIKI <dwiki.stw12@gmail.com>
2023-01-14 12:30:15 +0700

    MainActivity(First Screen) - add UI - add Feature

---
+++ app/src/main/java/com/dwiki/myapplication/ui/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.dwiki.myapplication.ui
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
+import com.dwiki.myapplication.R
+import com.dwiki.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+
+        binding.btnCheck.setOnClickListener {
+            if (binding.inputText.text.isNotEmpty()){
+                val result = isPolindrome(binding.inputText.text.toString())
+                setupDialog(binding.inputText.text.toString(), result)
+            }else{
+                binding.inputText.error = error
+            }
+        }
+
+        binding.btnNext.setOnClickListener {
+            if (binding.inputName.text.isNotEmpty()){
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }else{
+                binding.inputName.error = error
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun isPolindrome(text: String):Boolean{
+        val text2 = text.replace(" ", "")
+        val result = text2.reversed()
+        Log.d(TAG, result)
+
+        return text2 == result
+    }
+
+    private fun setupDialog(text: String, condition: Boolean){
+
+        val dialog = Dialog(this)
+
+        dialog.setTitle(null)
+        dialog.setCancelable(false)
+        dialog.setOnCancelListener(null)
+
+        dialog.setContentView(R.layout.dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val animation = dialog.findViewById<LottieAnimationView>(R.id.animationView)
+        val word = dialog.findViewById<TextView>(R.id.text)
+        val textResult = dialog.findViewById<TextView>(R.id.tvResult)
+        val btnClose = dialog.findViewById<ImageButton>(R.id.btnClose)
+
+        //setResult
+       if (condition){
+            textResult.text = "Is Polindrome"
+            animation.setAnimation(R.raw.right)
+        } else {
+            textResult.text = "Not Polindrome"
+            animation.setAnimation(R.raw.wrong)
+        }
+
+        word.text = "\"$text\""
+
+        dialog.show()
+
+        btnClose.setOnClickListener {
+            dialog.cancel()
+        }
+    }
+
+    companion object{
+        const val TAG = "MainActivity"
+        const val error = "it's empty"
+    }
+}
---
+++ app/src/main/java/com/dwiki/myapplication/ui/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.dwiki.myapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dwiki.myapplication.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
